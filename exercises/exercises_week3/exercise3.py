833b657acdc2ed05ffef469e4ca743dc197a2920
Petri Irri <petri.irri@gmail.com>
2021-02-05 13:21:03 +0200

    exercise3: count of odd and even numbers

---
+++ exercises/exercises_week3/exercise3.py
@@ -0,0 +1,11 @@
+# Count how many even and odd numbers there are in a given list.
+numbers = (3, 1, 6, 3, 4, 7, 0, 6, 3, 2, 6, 8, 3)
+odd = 0
+even = 0
+for number in numbers:
+    if number % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print("there were " + str(even) + " even numbers")
+print(odd)
