3531bfa6d3393f6172103ffb1b91407929044842
Petri Irri <petri.irri@gmail.com>
2021-02-12 13:07:32 +0200

    exercise2: functions that return the smallest or the biggest given number

---
+++ exercises/exercises_week4/exercise2.py
@@ -0,0 +1,22 @@
+def biggest(*numbers):
+    if numbers:
+        result = 0
+        for number in numbers:
+            if number > result:
+                result = number
+        return result
+    else:
+        return "No numbers given"
+
+def smallest(*numbers):
+    if numbers:
+        result = 0
+        for number in numbers:
+            if number < result:
+                result = number
+        return result
+    else:
+        return "No numbers given"
+
+print("Biggest number is:",biggest(1,2,3,4))
+print("Smallest number is:", smallest(1,2,-3,4))
